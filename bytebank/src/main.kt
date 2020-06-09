9951bad0673b0180b8a632513e4c8942b29a7244
Gabriel Alan <alang.von@gmail.com>
2020-06-08 23:16:12 -0300

    Operações Aritméticas

--- bytebank/src/main.kt
+++ bytebank/src/main.kt
@@ -1,5 +1,13 @@
 fun main(){
     println("Bem vindo ao Bytebank!")
-    val titular = "Gabriel"
+    val titular: String = "Gabriel"
+    val numeroConta: Int = 1000
+    var saldo: Double = 0.0
+
+    saldo = 100 + 2.0
+    saldo += 200
+
     println("Titular $titular")
+    println("Número da conta $numeroConta")
+    println("Saldo da conta $saldo")
 }
